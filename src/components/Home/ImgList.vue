cdef77f383ae96289af3dddbf1c7d5b1fcfea8b7
白褶 <919041098@qq.com>
2021-08-23 08:40:01 +0800

    uploadPhoto

--- src/components/Home/ImgList.vue
+++ src/components/Home/ImgList.vue
@@ -6,11 +6,17 @@
       </li>
     </ul>
     <ul class="rightList">
-      <Button type="add"><i class="iconfont icon-addPhoto" style="font-size: 20px"></i>自己传图</Button>
+      <Button type="add">
+        <label>
+        <span> <i class="iconfont icon-addPhoto" style="font-size: 20px"></i>
+        自己传图</span>
+          <input type="file" id="inputFile" @change="photoUpload">
+        </label>
+      </Button>
       <li class="list-item" v-for="item in RightImgList" :key="item">
         <img :src="item" alt="">
       </li>
-      <Button type="more" @click.native="ToPhotoGallery">更多美图</Button>
+      <Button type="more" @click.native="toPhotoGallery">更多美图</Button>
     </ul>
 
   </div>
@@ -18,18 +24,42 @@
 <script>
 import Button from "../Button"
 import {mapGetters} from "vuex"
+
 export default {
   components: {Button},
   data() {
     return {}
   },
   computed: {
-    ...mapGetters(['LeftImgList','RightImgList'])
+    ...mapGetters(['LeftImgList', 'RightImgList'])
   },
-  methods:{
-    ToPhotoGallery(){
-      console.log('click')
+  methods: {
+    toPhotoGallery() {
       this.$router.push('/photo-gallery')
+    },
+    photoUpload(event) {
+      const h = this.$createElement;
+      let file = event.target.files[0]
+      let suffixName = file.name.substring(file.name.lastIndexOf('.') + 1)
+      if (suffixName !== 'jpg' && suffixName !== 'png') {
+        this.$notify({
+          title: '警告',
+          message: h('i', {style: 'color: teal'}, '图片格式不正确')
+        });
+      }
+      let limitSize = file / 1024 / 1024 < 2
+      if (!limitSize) {
+        this.$notify({
+          title: '警告',
+          message: h('i', {style: 'color: teal'}, '上传文件不能超过2MB！')
+        });
+      }
+      let formData = new FormData();
+      formData.append('photoName',file)
+      this.$notify({
+        title: '警告',
+        message: h('i', {style: 'color: teal'}, '目前无上传接口！')
+      });
     }
   }
 }
@@ -40,16 +70,24 @@ export default {
 .list-wrapper {
   display: flex;
   justify-content: center;
-  .leftList{
+
+  .leftList {
     display: flex;
     flex-direction: column;
   }
-  .rightList{
+
+  .rightList {
     display: flex;
     flex-direction: column;
   }
+
+  #inputFile {
+    display: none;
+  }
+
   .list-item {
     padding: 4px 4px;
+
     img {
       vertical-align: bottom;
     }
